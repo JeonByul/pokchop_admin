562375af1a83ec1e8040267ef8e717c233ce29bb
Han-Byul Kim <shine_star_11@Han-Byuls-MacBook-Pro.local>
2016-07-25 04:43:35 +0900

    spinner2

--- app/src/main/java/com/shine_star_11/abc/MainActivity.java
+++ app/src/main/java/com/shine_star_11/abc/MainActivity.java
@@ -238,7 +238,9 @@ public class MainActivity extends AppCompatActivity
         behavior.setHideable(true);
         behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
-        String[] textArr = {"#001.Bulbasaur","#002.Ivysaur","#003.Venusaur","#004.Charmander","#005,Charmeleon","#006.Charizard"};
+        String[] textArr = {"#001.Bulbasaur","#002.Ivysaur","#003.Venusaur","#004.Charmander","#005,Charmeleon","#006.Charizard",
+        "#007.Squirtle","#008.Wartortle","#009.Blastoise","#010.Caterpie","#011.Metapod","#012.Butterfree","#013.Weedle","#014.Kakuna",
+        "#015.Beedrill"};
 
         Spinner mySpinner = (Spinner)findViewById(R.id.pokemon_name);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
